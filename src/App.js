16be4f37ab6570a422999cd7c0c2fd850b491e3d
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-07-19 16:16:56 -0700

    added useState from hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+     const [age,setAge]=useState(20);
+     const [name ,setName]= useState({firstname:'bhagya',lastname:'kavati'});
+     const handleClick = () => {
+          setAge(age+1);
+     }
+     
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>my full name is {name.firstname}{name.lastname}</p>
+      <p>click to increse  my age {age}</p>
+     <button onClick={handleClick}>click me</button>
     </div>
   );
 }
