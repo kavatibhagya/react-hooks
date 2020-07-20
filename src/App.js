244f81a3129ae68cadab92125bdd328307d86016
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-07-19 17:44:11 -0700

    adding useContext

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext, useContext } from 'react';
 import './App.css';
 
+
+
+const numberContext = React.createContext();
+
 function App() {
+
+      
+
 
      const [age,setAge]=useState(20);
      const [name ,setName]= useState({firstname:'bhagya',lastname:'kavati'});
@@ -20,11 +27,22 @@ function App() {
 
   return (
     <div className="App">
+      <numberContext.Provider value={50}>
+        <Display/>
+
+      </numberContext.Provider>
       <p>my full name is {name.firstname}{name.lastname}</p>
       <p>click to increse  my age {age}</p>
-     <button onClick={handleClick}>click me</button>
+     <button onClick={handleClick}>click to increse</button>
     </div>
   );
 }
+
+function Display(){
+
+  const value = useContext(numberContext);
+  return(
+    <div>the number is {value}</div>
+)}
 
 export default App;
